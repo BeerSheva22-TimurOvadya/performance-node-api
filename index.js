e0a10537872e68210b164b72b077717e5420f89f
Ovadya33 <timurgts2710@gmail.com>
2023-10-09 14:47:07 +0300

    123

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 8181;
 const server = app.listen(port);
-server.on('listening', () => console.log(`server is listening on port ${port}`));
+server.on('listening', () => console.log(`server is listening on port ${port}; process id: ${process.pid}`));
 const dbConnection = new MongoConnection(
     `mongodb+srv://root:${process.env.MONGO_PASSWORD}@cluster0.dojwwf6.mongodb.net/college?retryWrites=true&w=majority`,
     'college',
@@ -17,13 +17,14 @@ app.get('/performance/total', (req, res) => {
     for (let i = 0; i < count; i++) {
         total++;
     }
-    res.send({ api: 'node', total, time: new Date().getTime() - startTime.getTime() });
+    res.send({ pid: process.pid, api: 'node', total, time: new Date().getTime() - startTime.getTime() });
 });
 app.get('/performance/students', async (req, res) => {
     const startTime = new Date();
     const students = await studentsCollection.find({}).toArray();
 
     res.send({
+       pid: process.pid,
         api: 'node',
         total: students.length,
         time: new Date().getTime() - startTime.getTime(),
